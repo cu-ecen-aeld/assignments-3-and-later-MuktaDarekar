a210e0ae687bb2e68ef6de9eca9490b0ee6953be
MuktaDarekar <mukta.darekar@colorado.edu>
2021-08-31 23:51:25 -0600

    modified as suggested

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -19,5 +19,7 @@ void test_validate_my_username()
      * config file and my_username() functions are setup properly
      */
     //TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
-    TEST_ASSERT_EQUAL_STRING_MESSAGE(my_username(), malloc_username_from_conf_file(), "MuktaDarekar");
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(my_username(), malloc_username_from_conf_file(), "Username doesn't match");
+    
+    free(malloc_username_from_conf_file());
 }
